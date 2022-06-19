0ce549eb4e74564bd5cbc9ddaf5f15419586ad6c
Rohit Patil <91825947+rohitspatil30@users.noreply.github.com>
2022-06-19 17:54:18 +0530

    Add files via upload

---
+++ first.sql
@@ -0,0 +1,13 @@
+show databases;
+use result;
+show tables;
+truncate marks;
+insert into marks
+values (1,'CSBS','Rohit','patil');
+insert into marks
+values (2,'CSBS','sarthak','sonar');
+insert into marks
+values (3,'CSBS','vedant','patil');
+select * from marks;
+
+
